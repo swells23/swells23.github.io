f67957aabe4fa2f8a6536c69d59e7335f0a5b612
swells <wells.sn2@gmail.com>
2022-08-26 13:45:26 -0400

    add repo link to nav

--- src/components/pattern/GlobalNav/GlobalNav.js
+++ src/components/pattern/GlobalNav/GlobalNav.js
@@ -2,15 +2,20 @@ import {
   AppBar,
   BottomNavigation,
   BottomNavigationAction,
+  Link,
   Tab,
   Tabs,
+  Toolbar,
+  Typography,
   useMediaQuery,
   useTheme
 } from '@mui/material';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import { navigate } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { PAGELIST } from '../../../data/templateMeta';
+import styles from './GlobalNav.styles';
 
 const GlobalNav = ({ location }) => {
   const [value, setValue] = React.useState(location.pathname),
@@ -41,16 +46,27 @@ const GlobalNav = ({ location }) => {
     )
     : (
       <AppBar position='sticky'>
-        <Tabs
-          component='nav'
-          aria-label='navigation'
-          value={value}
-          onChange={handleChange}
-          textColor='inherit'
-          indicatorColor='secondary'
-        >
-          {getMenuItems()}
-        </Tabs>
+        <Toolbar sx={styles.toolbar}>
+          <Tabs
+            component='nav'
+            aria-label='navigation'
+            value={value}
+            onChange={handleChange}
+            textColor='inherit'
+            indicatorColor='secondary'
+            sx={styles.tabs}
+          >
+            {getMenuItems()}
+          </Tabs>
+          <Link
+            href='https://github.com/swells23/swells23.github.io'
+            color='secondary'
+            underline='hover'
+            target='_blank'>
+            View Repository
+              <OpenInNewIcon sx={styles.repoLink} />
+          </Link>
+        </Toolbar>
       </AppBar>
     );
 };
---
+++ src/components/pattern/GlobalNav/GlobalNav.styles.js
@@ -0,0 +1,16 @@
+const styles = {
+    toolbar: {
+        '&.MuiToolbar-root': {
+            minHeight: 'unset'
+        }
+    },
+    tabs: {
+        flexGrow: 1
+    },
+    repoLink: {
+        marginLeft: '.25rem',
+        verticalAlign: 'middle'
+    }
+};
+
+export default styles;
